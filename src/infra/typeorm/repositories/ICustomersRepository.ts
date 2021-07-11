198cf993941f18c2653c456a0459818529ef78c9
Bruno Cheble <contato.cheble@gmail.com>
2021-07-11 01:52:21 +0100

    Fix importing interface

--- src/infra/typeorm/repositories/ICustomersRepository.ts
+++ src/infra/typeorm/repositories/ICustomersRepository.ts
@@ -1,5 +1,4 @@
 import IFindAllCustomerByCountryDTO from '../../../dtos/IFindAllCustomerByCountryDTO';
-import IFindAllCustomerDTO from '../../../dtos/IFindAllCustomerDTO';
 import Customer from '../entities/Customer';
 
 export default interface ICustomersRepository {
